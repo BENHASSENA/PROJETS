3bec8d0f1f7a09b0ec9832a91ca60b79be574751
PREPA-B\Prepa <houriabenhassena@gmail.com>
2021-02-08 16:04:10 +0100

    push

---
+++ main.js
@@ -0,0 +1,35 @@
+console.log('hello');
+
+const addButton = document.querySelector('.btn-add');  // la variable addButton selectionne la classe .btn-add
+const suppButton = document.querySelector('.btn-supp')  // la variable suppButton selectionne la classe .btn-supp
+const userList = document.querySelector('#userlist');  // la variable userList selectionne l'id userlist
+const nameInput = document.querySelector('#Name');  // la variable nameInput selectionne l'id Name
+const emailInput = document.querySelector('#Email');  // la variable emailInput selectionne l'id Email
+
+console.log(window);
+
+addButton.addEventListener('click',(e) =>{  // 
+
+        e.preventDefault();
+
+        const li = document.createElement('li'); // la variable li créé la balise li
+
+        li.className = "user";  // je donne une class user à la li
+
+        li.appendChild(document.createTextNode(`${nameInput.value} : ${emailInput.value}`));
+        // je récupere la valeur de l'id Name et de l'id Email avec lesquelles je créé un contenu temporaire que je vais insérer dans le li
+
+        if(userList.appendChild(li)){ 
+            nameInput.value = ' ';
+            emailInput.value = ' ';
+        }
+
+});
+
+
+suppButton.addEventListener('click',(e) =>{
+
+        li.remove;
+
+
+});
